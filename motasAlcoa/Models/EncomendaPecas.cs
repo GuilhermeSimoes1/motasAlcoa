bf655015b426ef0e3d7fa545c6d123921d1e3b68
Guilherme Simões <137619185+GuilhermeSimoes1@users.noreply.github.com>
2025-03-19 15:19:33 +0000

    Criação dos models

---
+++ motasAlcoa/Models/EncomendaPecas.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace motasAlcoa.Models
+{
+
+    /// <summary>
+    /// Representa a relação entre uma encomenda e as peças associadas.
+    /// Cada instância desta classe indica a quantidade de uma peça específica 
+    /// dentro de uma determinada encomenda.
+    /// </summary>
+
+    [PrimaryKey(nameof(EncomendaFK), nameof(PecaFK))]
+    public class EncomendaPecas
+    {
+        /// <summary>
+        /// Identificador da encomenda
+        /// </summary>
+        [ForeignKey(nameof(Encomenda))]
+        public int EncomendaFK { get; set; }
+
+        /// <summary>
+        /// Fk para a encomenda
+        /// </summary>
+        public Encomendas Encomenda { get; set; }
+
+        /// <summary>
+        /// Identificador da peça
+        /// </summary>
+        [ForeignKey(nameof(Peca))]
+        public int PecaFK { get; set; }
+
+        /// <summary>
+        /// FK para a peça
+        /// </summary>
+        public Pecas Peca {  get; set; }
+
+        /// <summary>
+        /// Número de unidades da peça solicitada no pedido
+        /// </summary>
+        public int Quantidade{ get; set; }
+
+    }
+}
---
+++ motasAlcoa/Models/ServicoPecas.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace motasAlcoa.Models
+{
+    ///
+    /// <summary>
+    /// Representa a relação entre os serviços realizados e as peças utilizadas.
+    /// Cada registro nesta tabela indica quantas unidades de uma peça foram usadas
+    /// num serviço específico.
+    /// </summary>
+    /// 
+
+    [PrimaryKey(nameof(ServicosFK), nameof(PecaFK))]
+    public class ServicoPecas
+    {
+        /// <summary>
+        /// Identificador do serviço que está associado 
+        /// ao relacionamento entre serviços e peças
+        /// </summary>
+ 
+        [ForeignKey(nameof(Servico))]
+        public int ServicosFK { get; set; }
+
+        /// <summary>
+        /// FK para os serviços
+        /// </summary>
+        public Servicos Servico { get; set; }
+
+
+        /// <summary>
+        /// Identificador da peça que está associada    
+        /// ao relacionamento entre serviços e peças
+        /// </summary>
+
+        [ForeignKey(nameof(Peca))]
+        public int PecaFK { get; set; }
+
+        /// <summary>
+        /// FK para as peças
+        /// </summary>
+        public Pecas Peca {  get; set; }
+
+        /// <summary>
+        /// Quantidade da peça usada no serviço
+        /// </summary>
+        [Required]
+        public int QuantidadeUsada { get; set; }
+
+
+
+    }
+}
